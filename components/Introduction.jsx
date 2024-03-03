fd8bb2c17a7427998430171955354f021b83cf48
zhang-yuangg <2232945838@qq.com>
2024-03-03 12:11:00 +0800

    adjust the image

--- components/Introduction.jsx
+++ components/Introduction.jsx
@@ -10,7 +10,7 @@ function Introduction() {
   return (
     <MaxWidthWrapper className=" mb-12 mt-15 sm:mt-20 lg:mt-36 flex justify-between items-center flex-col sm:flex-row gap-10">
       <div className=" sm:hidden ring-1 ring-inset ring-gray-900/10 p-2 bg-gray-900/5 rounded-md">
-        <div className=" w-[20dvh]">
+        <div className=" w-44">
           <Image
             src={'/images/main_photo.png'}
             alt="people"
